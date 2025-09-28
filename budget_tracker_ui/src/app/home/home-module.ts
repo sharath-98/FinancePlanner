5854509d4e5442b282689a85b2306d98e94a0d50
sharath-98 <sharathchander.p@gmail.com>
2025-09-27 20:32:49 -0400

    Date format fixes

--- budget_tracker_ui/src/app/home/home-module.ts
+++ budget_tracker_ui/src/app/home/home-module.ts
@@ -4,8 +4,23 @@ import { AgCharts, AgChartsModule } from 'ag-charts-angular';
 import { AgGridModule } from 'ag-grid-angular';
 import { Home } from './home';
 import { MaterialModule } from '../material-module';
-import { MatMomentDateModule } from '@angular/material-moment-adapter';
+import { MatMomentDateModule, MomentDateAdapter } from '@angular/material-moment-adapter';
 import { ReactiveFormsModule } from '@angular/forms';
+import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
+import * as _moment from 'moment';
+import { MatNativeDateModule } from '@angular/material/core';
+
+const moment = _moment;
+
+export const MY_FORMATS = {
+  parse: { dateInput: 'MM/YYYY' },
+  display: {
+    dateInput: 'MM/YYYY',
+    monthYearLabel: 'MMM YYYY',
+    dateA11yLabel: 'LL',
+    monthYearA11yLabel: 'MMMM YYYY',
+  },
+};
 
 @NgModule({
   declarations: [Home],
@@ -16,7 +31,11 @@ import { ReactiveFormsModule } from '@angular/forms';
     AgChartsModule,
     MaterialModule,
     MatMomentDateModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+  ],
+  providers: [
+    { provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE] },
+    { provide: MAT_DATE_FORMATS, useValue: MY_FORMATS },
   ],
 })
 export class HomeModule {}
